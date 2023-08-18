c8f6f9a14c3669ac16945712ce5b945c68260c82
root0beer <tanyajackdaw@gmail.com>
2023-08-18 09:24:19 +0300

    meta tags for index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,36 +6,59 @@ const HomePage = () => {
     <>
       <Head>
         {/* Essential Meta Tags */}
-        <title>Iola</title>
-        <meta name="description" content="Your page description." />
+        <title>Irving & Otto</title>
+        <meta
+          name="description"
+          content="Explore a world of timeless elegance and curated charm at Irving & Otto. Discover a collection of unique antique home decor pieces, meticulously selected to adorn your living spaces in an eclectic style that exudes sophistication and character."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="canonical" href="https://yourwebsite.com/" />
+        <link rel="canonical" href="https://irvingandotto.netlify.app/" />
 
         {/* Open Graph and Twitter Cards */}
-        <meta property="og:title" content="Your Open Graph Title" />
+        <meta
+          property="og:title"
+          content="Irving & Otto: Antique Home Decor in Eclectic Elegance"
+        />
         <meta
           property="og:description"
-          content="Your Open Graph Description."
+          content="Indulge in the opulence of Irving & Otto, where the allure of antique home decor meets eclectic sophistication. Immerse yourself in a collection of unique pieces that capture the essence of timeless charm."
         />
         <meta
           property="og:image"
           content="https://yourwebsite.com/og-image.jpg"
         />
-        <meta property="og:url" content="https://yourwebsite.com/" />
+        <meta property="og:url" content="https://irvingandotto.netlify.app/" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourtwitterhandle" />
-        <meta name="twitter:creator" content="@yourtwitterhandle" />
+        <meta name="twitter:site" content="@irvingandotto" />
+        <meta name="twitter:creator" content="@irvingandotto" />
 
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
-
-        {/* CSS Stylesheets */}
-        <link rel="stylesheet" href="/styles.css" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
 
         {/* Additional Meta Tags */}
         <meta name="robots" content="index, follow" />
-        <meta name="keywords" content="your, keywords, here" />
-        <meta name="author" content="Your Name" />
+        <meta
+          name="keywords"
+          content="antique home decor, eclectic style, luxury decor"
+        />
+        <meta name="author" content="Irving & Otto" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
     </>
