76fa9fe236441ef7f30f4a7619137e792501c519
root0beer <tanyajackdaw@gmail.com>
2023-08-18 22:12:07 +0300

    content placement for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,11 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <div className={styles.wrapperFooter}>
-        
+        <div className={styles.quoteBlock}>
+            <h2 className={styles.quoteText}>Each antique piece carries the legacy of the hands that crafted it and the souls that cherished it.</h2>
+            <h1 className={styles.quoteOpen}>“</h1>
+            <h1 className={styles.quoteClose}>”</h1>
+        </div>
     </div>
   )
 };
