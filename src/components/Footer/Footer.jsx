515a0e5a23f0510395f09117a26cc82b53be80a4
root0beer <tanyajackdaw@gmail.com>
2023-08-18 22:35:00 +0300

    styles for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React from "react";
 import styles from "./Footer.module.scss";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.wrapperFooter}>
-        <div className={styles.quoteBlock}>
-            <h2 className={styles.quoteText}>Each antique piece carries the legacy of the hands that crafted it and the souls that cherished it.</h2>
-            <h1 className={styles.quoteOpen}>“</h1>
-            <h1 className={styles.quoteClose}>”</h1>
-        </div>
+      <div className={styles.quoteBlock}>
+        <h2 className={styles.quoteText}>
+          Each antique piece carries the legacy of the hands that crafted it and
+          the souls that cherished it.
+        </h2>
+        <Image
+          className={styles.quoteOpen}
+          src={"/quoteopen.png"}
+          alt={"quoteopen"}
+          width={74}
+          height={67}
+          quality={100}
+        />
+        <Image
+          className={styles.quoteClose}
+          src={"/quoteclose.png"}
+          alt={"quoteclose"}
+          width={74}
+          height={67}
+          quality={100}
+        />
+      </div>
     </div>
-  )
+  );
 };
 
 export default Footer;
