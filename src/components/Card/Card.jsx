f4bb4add69e58d7a00d77aa7f425a1a180f08ea3
root0beer <tanyajackdaw@gmail.com>
2023-08-22 20:07:08 +0300

    card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Card.module.scss";
+
+const Card = () => {
+  return (
+    <>
+      <div className={styles.layoutCart}></div>
+      <div className={styles.cartWrapper}>
+        <div ></div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
