13c268981d01e64865e6a073190e4c67261785f3
root0beer <tanyajackdaw@gmail.com>
2023-08-23 10:30:05 +0300

    cart styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,42 +9,52 @@ const Cart = ({ opened, onClose }) => {
         <div
           className={`${styles.overlay} ${opened ? styles.overlayVisible : ""}`}
         >
-          <div className={styles.cartWrapper} >
-            <div className={styles.navBarCart}>
-              <h2 className={styles.cartTitle}>Cart</h2>
-              <p className={styles.cartClose} onClick={onClose}>Close</p>
-            </div>
-            <div className={styles.cartItemBlock}>
-              <div className={styles.itemImageBlock}>
-                <Image
-                  className={styles.itemImage}
-                  src={"/content/product2.png"}
-                  width={274}
-                  height={183}
-                  placeholder="blur"
-                  blurDataURL="/content/product2Blur.png"
-                />
+          <div className={styles.cartWrapper}>
+            <div className={styles.cartPaddings}>
+              <div className={styles.navBarCart}>
+                <h2 className={styles.cartTitle}>Cart</h2>
+                <p className={styles.cartClose} onClick={onClose}>
+                  Close
+                </p>
               </div>
-              <div className={styles.descriptionBlock}>
-                <h4 className={styles.productTitle}>
-                  9th century style Tenerif Vase{" "}
-                </h4>
-                <p className={styles.price}>$ 1150</p>
-                <p className={styles.remove}>Remove</p>
-              </div>
-              <div className={styles.quantityBlock}>
-                <p className={styles.quantityTitle}>Quantity</p>
-                <div className={styles.plusMinusBlock}>
-                  <p className={styles.minusSign}>-</p>
-                  <p className={styles.quantityNumber}>1</p>
-                  <p className={styles.plusSign}>+</p>
+              <div className={styles.cartItemsList}>
+                <div className={styles.cartItemBlock}>
+                  <div className={styles.cartItemImageDescrBlock}>
+                    <div className={styles.itemImageBlock}>
+                      <Image
+                        className={styles.itemImage}
+                        src={"/content/product2.png"}
+                        width={274}
+                        height={183}
+                        placeholder="blur"
+                        blurDataURL="/content/product2Blur.png"
+                      />
+                    </div>
+                    <div className={styles.descriptionBlock}>
+                      <h4 className={styles.productTitle}>
+                        9th century style Tenerif Vase{" "}
+                      </h4>
+                      <p className={styles.price}>$ 1150</p>
+                      <p className={styles.remove}>Remove</p>
+                    </div>
+                  </div>
+                  <div className={styles.quantityBlock}>
+                    <p className={styles.quantityTitle}>Quantity</p>
+                    <div className={styles.plusMinusBlock}>
+                      <p className={styles.minusSign}>-</p>
+                      <p className={styles.quantityNumber}>1</p>
+                      <p className={styles.plusSign}>+</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className={styles.checkoutBlock}>
-              <div className={styles.checkoutPriceBlock}>
-                <p className={styles.total}>Total:</p>
-                <p className={styles.totalPrice}>$8266</p>
+            <div>
+              <div className={styles.checkoutBlock}>
+                <div className={styles.checkoutPriceBlock}>
+                  <p className={styles.total}>Total:</p>
+                  <p className={styles.totalPrice}>$8266</p>
+                </div>
               </div>
               <button className={styles.checkoutBtn}>Checkout</button>
             </div>
