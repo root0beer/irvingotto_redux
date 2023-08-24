e5ec56296500ca665b9fd894aa521adb9f71e2b9
root0beer <tanyajackdaw@gmail.com>
2023-08-24 15:30:20 +0300

    removing cart item from the cart logic, or reducing the quantity by 1, in reducer in cart-slice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
     name: "cart",
+    //here we basically state what we want to display on the screen :\ in simpler terms
     initialState: {
         cartItems: [],
         totalPrice: 0,
@@ -20,6 +21,7 @@ const cartSlice = createSlice({
                     //since we are adding it for the first time, its 1:
                     cartItemQuantity: 1,
                     //since we are adding it for the first time, total price is just price:
+                    //we can store total cart price here yea:
                     totalCartPrice: newCartItem.price,
                     cartItemTitle: newCartItem.title,
                 });
@@ -28,7 +30,23 @@ const cartSlice = createSlice({
                 existingCartItem.totalCartPrice = existingCartItem.totalCartPrice + newCartItem.cartItemPrice;
             }
         },
-        removeItemFromCart() {},
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            //we are finding that item and remembering it in the existingCartItem
+            const existingCartItem = state.cartItems.find(item => item.id === id);
+            state.totalPrice = existingCartItem.totalCartPrice - existingCartItem.cartItemPrice;
+            //if the quantity of the item is 1 we want to remove it entirely
+            //if its greater than 1, we watnt to reduce the quantity by 1:
+            if (existingCartItem.cartItemQuantity === 1) {
+                //we are filtering all the items out where item id is NOT equal to the id,
+                //we keeping all the items where id's didnt match
+                state.cartItems = state.cartItems.filter(item => item.id !== id);
+            } else {
+                existingCartItem.cartItemQuantity--;
+                //calculating total price of the items in the cart
+                existingCartItem.totalCartPrice = existingCartItem.totalCartPrice - existingCartItem.cartItemPrice;
+            }
+        },
     },
 });
 
