db21dda1f1c2681a668ce3b21644a5c578871fc6
root0beer <tanyajackdaw@gmail.com>
2023-08-27 11:05:34 +0300

    adjusting data mutation to send images

--- src/pages/api/favourites.js
+++ src/pages/api/favourites.js
@@ -10,13 +10,15 @@ export default async function comments(req, res) {
     });
     
     const favQuery = gql`
-    mutation CreateFavourite($title: String!) {
-        createFavourite(data: {title: $title}) {id}
+    mutation CreateFavourite($title: String!, $image: Upload!, $imageBlur: Upload!) {
+        createFavourite(data: {title: $title, image: $image, imageBlur: $imageBlur}) {id}
     }
     `;
 
     const result = await graphQLClient.request(favQuery, {
         title: req.body.title,
+        image: req.body.image,
+        imageBlur: req.body.imageBlur,
     });
     return res.status(200).send(result);
 };
