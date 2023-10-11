1b51dd4d916ff07275be6f3e53eafe9fa005d250
root0beer <tanyajackdaw@gmail.com>
2023-10-11 21:37:04 +0300

    removing old api code

--- src/pages/api/favourites.js
+++ src/pages/api/favourites.js
@@ -1,31 +1,31 @@
-import { GraphQLClient, gql } from "graphql-request";
+// import { GraphQLClient, gql } from "graphql-request";
 
-const url = process.env.ENDPOINT;
+// const url = process.env.ENDPOINT;
 
-export default async function favourites(req, res) {
-    const graphQLClient = new GraphQLClient((url), {
-        headers: {
-            authorization: process.env.CMS_TOKEN,
-        },
-    });
+// export default async function favourites(req, res) {
+//     const graphQLClient = new GraphQLClient((url), {
+//         headers: {
+//             authorization: process.env.CMS_TOKEN,
+//         },
+//     });
 
-    const favQuery = gql`
-    mutation CreateFavourite($title: String!, $image: String!, $imageBlur: String!, $imageId: String! ) {
-        createFavourite(data: {title: $title, image: $image, imageBlur: $imageBlur, imageId: $imageId}) {
-            id 
-            title
-            image
-            imageBlur
-            imageId
-        }
-    }
-    `;
+//     const favQuery = gql`
+//     mutation CreateFavourite($title: String!, $image: String!, $imageBlur: String!, $imageId: String! ) {
+//         createFavourite(data: {title: $title, image: $image, imageBlur: $imageBlur, imageId: $imageId}) {
+//             id 
+//             title
+//             image
+//             imageBlur
+//             imageId
+//         }
+//     }
+//     `;
 
-    const result = await graphQLClient.request(favQuery, {
-        title: req.body.title,
-        image: req.body.image,
-        imageBlur: req.body.imageBlur,
-        imageId: req.body.imageId,
-    });
-    return res.status(200).send(result);
-};
+//     const result = await graphQLClient.request(favQuery, {
+//         title: req.body.title,
+//         image: req.body.image,
+//         imageBlur: req.body.imageBlur,
+//         imageId: req.body.imageId,
+//     });
+//     return res.status(200).send(result);
+// };
